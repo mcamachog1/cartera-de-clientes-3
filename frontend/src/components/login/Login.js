704dd0a0980fa11f08185e32cbd1834f6f07d002
Maryvi <mcamachog@gmail.com>
2022-12-08 03:44:25 +0000

    diseno inicial pagina de sign up

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -3,11 +3,12 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 
 function Login () {
-        return (
-            <Container>
-                <h1>Login</h1>
-            </Container>
-        )
+
+	return (
+			<Container>
+					<h1>Login</h1>
+			</Container>
+	)
 }
 
 export default Login;
--- frontend/src/components/signup/Signup.js
+++ frontend/src/components/signup/Signup.js
@@ -1,12 +1,59 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Link } from "react-router-dom";
+import { 
+  Container,
+	Button,
+	Row,
+	Col,
+	Form,
+	FormControl
+} from 'react-bootstrap'
 
-function Signup () {
-        return (
-            <Container>
-                <h1>Signup</h1>
-            </Container>
-        )
+function Signup (props) {
+
+	const [credenciales, setCredenciales] = useState( {username: "", password: ""});
+  return (
+	<Container>
+		<Row>
+			<Col md="4">
+				<h1>Sign up</h1>
+				<Form>
+					<Form.Group controlId="usernameId">
+						<Form.Label>User name</Form.Label>
+						<Form.Control
+							type="text"
+							name="username"
+							placeholder="Enter user name"
+							value={props.username}
+							onChange={props.onChange}
+						/>
+						<FormControl.Feedback type="invalid"></FormControl.Feedback>
+					</Form.Group>
+
+					<Form.Group controlId="passwordId">
+						<Form.Label>Your password</Form.Label>
+						<Form.Control
+							type="password"
+							name="password"
+							placeholder="Enter password"
+							value={props.password}
+							onChange={props.onChange}
+						/>
+						<Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+					</Form.Group>
+				</Form>
+				<Button
+					className="mt-2"
+					color="primary"
+					onClick={props.onSignupClick}  
+				>Sign up</Button>
+				<p className="mt-2">
+					Already have account? <Link to="/login">Login</Link>
+				</p>
+			</Col>
+		</Row>
+	</Container>
+  )
 }
 
 export default Signup;
